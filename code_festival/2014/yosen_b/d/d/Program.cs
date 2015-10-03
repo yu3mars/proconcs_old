b387558e39d7dcb215132b50e349ca8ccd2f65b3
yu3mars <yu3mars@users.noreply.github.com>
2015-10-04 01:58:33 +0900

    fix code festival 2014 qualb d

--- code_festival/2014/yosen_b/d/d/Program.cs
+++ code_festival/2014/yosen_b/d/d/Program.cs
@@ -11,66 +11,33 @@ namespace d
         {
             int n = int.Parse(Console.ReadLine());
             int[] h = new int[n + 2];
-            int[] w = new int[n + 2];//hidarikara
-            int[] e = new int[n + 2];//migikara
-            bool[] checkw = new bool[n + 2];
-            bool[] checke = new bool[n + 2];
-            checkw[0] = true;
-            checke[0] = true;
-            checkw[n + 1] = true;
-            checke[n + 1] = true;
+            int[] l = new int[n + 2];
+            int[] r = new int[n + 2];
             h[0] = int.MaxValue;
             h[n + 1] = int.MaxValue;
-            Queue<int> qw = new Queue<int>();
-            Queue<int> qe = new Queue<int>();
             for (int i = 1; i <= n; i++)
             {
                 h[i] = int.Parse(Console.ReadLine());
             }
-            for (int i = 1; i <= n; i++)
+            for (int i = n; i > 0; i--)
             {
-                if (h[i] < h[i - 1])
+                r[i] = i + 1;
+                while (h[i] >= h[r[i]])
                 {
-                    qw.Enqueue(i);
-                    w[i] = 0;
-                }
-                if (h[i] < h[i + 1])
-                {
-                    qe.Enqueue(i);
-                    e[i] = 0;
-                }
-            }
-            while (qw.Count > 0)
-            {
-                int next = qw.Dequeue();
-                
-            }
-            for (int i = 1; i <= n; i++)
-            {
-                
-                if (checkw[i] == false && h[i] < h[i - 1])
-                {
-                    
-                }
-            }
-            for (int i = n; i >= 1; i--)
-            {
-                for (int j = i - 1; j >=1; j--)
-                {
-                    if (h[i] >= h[j])
-                    {
-                        e[i] += 1;
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    r[i] = r[r[i]];
                 }
             }
             for (int i = 1; i <= n; i++)
             {
-                int ans = w[i] + e[i];
-                Console.WriteLine(ans);
+                l[i] = i - 1;
+                while (h[i] >= h[l[i]])
+                {
+                    l[i] = l[l[i]];
+                }
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(r[i] - l[i] - 2);
             }
         }
     }
