3086495551ae51a02864ac2dee3727631917578b
yu3mars <yu3mars@users.noreply.github.com>
2015-10-25 16:07:49 +0900

    add kupc2015

---
+++ atcoder/kupc2015/a/a/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int ans = 0;
+                string s = Console.ReadLine();
+                for (int j = 0; j < s.Length-4; j++)
+                {
+                    if (s[j] == 't')
+                    {
+                        if (s.Substring(j, 5) == "tokyo")
+                        {
+                            ans++;
+                            j += 4;
+                        }
+                    } 
+                    else if (s[j] == 'k')
+                    {
+                        if (s.Substring(j, 5) == "kyoto")
+                        {
+                            ans++;
+                            j += 4;
+                        }
+                    }
+                }
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
---
+++ atcoder/kupc2015/b/b/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("..........");
+            Console.WriteLine("..........");
+            Console.WriteLine("...C......");
+            Console.WriteLine("..........");
+            Console.WriteLine("..........");
+            Console.WriteLine("..........");
+            Console.WriteLine("..CC..CC..");
+            Console.WriteLine("..........");
+            Console.WriteLine("..........");
+            Console.WriteLine("..........");
+        }
+    }
+}
